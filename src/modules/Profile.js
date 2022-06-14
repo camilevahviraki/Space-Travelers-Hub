cf71daa23bc1acd0d21f246b2a61ea80a11e562b
camilevahviraki <camilevahviraki@gmail.com>
2022-06-15 01:36:44 +0200

    display selected missions

--- src/modules/Profile.js
+++ src/modules/Profile.js
@@ -3,15 +3,25 @@ import { connect } from 'react-redux';
 
 function Profile(props) {
   const [profileRockets, setProfileRockets] = useState([]);
+  const [profileMissions, setProfileMissions] = useState([]);
 
   useEffect(() => {
     setProfileRockets(props.selectedRockets.RocketsReducer.profileRockets);
+    setProfileMissions(props.selectedRockets.MissionsReducer.profileMissions);
   }, []);
 
   return (
-    <div>
-      <h3>Rockets</h3>
+    <div className="profile">
+
       <div>
+        <h3>Missions</h3>
+        { profileMissions.map((mission) => (
+          <p key={mission.Missions[0]}>{mission.Missions[1].mission_name}</p>
+        )) }
+      </div>
+
+      <div>
+        <h3>Rockets</h3>
         { profileRockets.map((rocket) => (
           <p key={rocket.rocket[0]}>{rocket.rocket[1].rocket_name}</p>
         )) }
