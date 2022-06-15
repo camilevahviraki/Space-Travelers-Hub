308951d25c3d9fa4e7dfafe266b8b797a256ab00
camilevahviraki <camilevahviraki@gmail.com>
2022-06-15 16:42:49 +0200

    Update Missions

--- src/modules/Missions.js
+++ src/modules/Missions.js
@@ -1,29 +1,72 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, connect, useDispatch } from 'react-redux';
 import { displayMissions, reserveMission } from '../redux/Missions/missions';
+import '../styles/Mission.css';
 
 function Missions({ UpdatedMissions, getMissions }) {
   useEffect(() => {
     getMissions();
   }, []);
+
   const Missions = UpdatedMissions;
+  const MissionsDisplayed = useSelector((state) => state.MissionsReducer.newMissions);
+
   const dispatch = useDispatch();
+  const [newMissionU, setNewMissionU] = useState();
+  const [btnStatus, setBtnStatus] = useState(false);
+
+  const assignButton = (id) => {
+    if (Missions) {
+      return Missions[id].btn;
+    }
+    return 'Not a member';
+  };
+
+  const style1 = {
+    backgroundColor: 'transparent',
+  };
+
+  const style2 = {
+    backgroundColor: '#D3D3D3',
+  };
 
   return (
     <table>
-      {Missions.map((mission) => (
-        <tr key={mission.Missions[0]}>
-          <th>{mission.Missions[1].mission_name}</th>
-          <th>{mission.Missions[1].description}</th>
-          <th>
+      <thead> table</thead>
+      {MissionsDisplayed.map((mission, id) => (
+        <tr key={mission.Missions[0]} style={id % 2 === 0 ? style2 : style1}>
+          <th className="th_title">{mission.Missions[1].mission_name}</th>
+          <th className="th_desc">{mission.Missions[1].description}</th>
+          <th className="th_button">
             {
-                mission.reserved ? (<span>Active member</span>) : (<span>Not a member</span>)
+                mission.reserved ? (<span className="status1">Active member</span>) : (<span className="status2">Not a member</span>)
               }
           </th>
           <th>
-            <button type="button" onClick={() => dispatch(reserveMission(mission.Missions[0]))}>
-              {mission.reserved ? 'Join Mission' : 'Leave Mission' }
-            </button>
+            {mission.reserved ? (
+              <button
+                className="button_Leave"
+                type="button"
+                onClick={() => {
+                  setNewMissionU(UpdatedMissions);
+                  return dispatch(reserveMission(mission.Missions[0]));
+                }}
+              >
+                Join Mission
+              </button>
+            ) : (
+              <button
+                className="button_Join"
+                type="button"
+                onClick={() => {
+                  setNewMissionU(UpdatedMissions);
+                  return dispatch(reserveMission(mission.Missions[0]));
+                }}
+              >
+                Leave Mission
+              </button>
+            )}
+
           </th>
         </tr>
       ))}
@@ -31,7 +74,7 @@ function Missions({ UpdatedMissions, getMissions }) {
   );
 }
 
-const mapStateToProps = (state) => ({ UpdatedMissions: state.MissionsReducer.newMissions });
+const mapStateToProps = (state) => ({ UpdatedMissions: state });
 
 const mapDispatchToProps = (dispatch) => ({ getMissions: () => dispatch(displayMissions()) });
 
