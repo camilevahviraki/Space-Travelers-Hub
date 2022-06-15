856fb492ea35e0f94a85f190074cf75eeba8f26e
camilevahviraki <camilevahviraki@gmail.com>
2022-06-15 20:56:51 +0200

    update profile design

--- src/modules/Missions.js
+++ src/modules/Missions.js
@@ -1,26 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, connect, useDispatch } from 'react-redux';
 import { displayMissions, reserveMission } from '../redux/Missions/missions';
 import '../styles/Mission.css';
 
-function Missions({ UpdatedMissions, getMissions }) {
+function Missions({ getMissions }) {
   useEffect(() => {
     getMissions();
   }, []);
 
-  const Missions = UpdatedMissions;
   const MissionsDisplayed = useSelector((state) => state.MissionsReducer.newMissions);
 
   const dispatch = useDispatch();
-  const [newMissionU, setNewMissionU] = useState();
-  const [btnStatus, setBtnStatus] = useState(false);
-
-  const assignButton = (id) => {
-    if (Missions) {
-      return Missions[id].btn;
-    }
-    return 'Not a member';
-  };
 
   const style1 = {
     backgroundColor: 'transparent',
@@ -32,44 +22,46 @@ function Missions({ UpdatedMissions, getMissions }) {
 
   return (
     <table>
-      <thead> table</thead>
-      {MissionsDisplayed.map((mission, id) => (
-        <tr key={mission.Missions[0]} style={id % 2 === 0 ? style2 : style1}>
-          <th className="th_title">{mission.Missions[1].mission_name}</th>
-          <th className="th_desc">{mission.Missions[1].description}</th>
-          <th className="th_button">
-            {
+      <thead>
+        <tr>
+          <th className="th_title">Mission</th>
+          <th className="th_desc desc_head">Description</th>
+          <th className="th_button">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {MissionsDisplayed.map((mission, id) => (
+          <tr key={mission.Missions[0]} style={id % 2 === 0 ? style2 : style1}>
+            <td className="th_title desc_head">{mission.Missions[1].mission_name}</td>
+            <td className="th_desc">{mission.Missions[1].description}</td>
+            <td className="th_button">
+              {
                 mission.reserved ? (<span className="status1">Active member</span>) : (<span className="status2">Not a member</span>)
               }
-          </th>
-          <th>
-            {mission.reserved ? (
-              <button
-                className="button_Leave"
-                type="button"
-                onClick={() => {
-                  setNewMissionU(UpdatedMissions);
-                  return dispatch(reserveMission(mission.Missions[0]));
-                }}
-              >
-                Join Mission
-              </button>
-            ) : (
-              <button
-                className="button_Join"
-                type="button"
-                onClick={() => {
-                  setNewMissionU(UpdatedMissions);
-                  return dispatch(reserveMission(mission.Missions[0]));
-                }}
-              >
-                Leave Mission
-              </button>
-            )}
+            </td>
+            <td>
+              {mission.reserved ? (
+                <button
+                  className="button_Leave"
+                  type="button"
+                  onClick={() => dispatch(reserveMission(mission.Missions[0]))}
+                >
+                  Leave Mission
+                </button>
+              ) : (
+                <button
+                  className="button_Join"
+                  type="button"
+                  onClick={() => dispatch(reserveMission(mission.Missions[0]))}
+                >
+                  Join Mission
+                </button>
+              )}
 
-          </th>
-        </tr>
-      ))}
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
